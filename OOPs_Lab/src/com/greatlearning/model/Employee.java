b4ca5c33c91aa803c65881097ee28eda80cae98c
wittin <wittinprince@gmail.com>
2022-06-19 19:28:06 +0530

    commit 1

--- OOPs_Lab/src/com/greatlearning/model/Employee.java
+++ OOPs_Lab/src/com/greatlearning/model/Employee.java
@@ -1,4 +1,6 @@
-package com.greatlearning.OOPsLab;
+package com.greatlearning.model;
+
+import java.util.Objects;
 
 public class Employee {
 
@@ -35,6 +37,24 @@ public class Employee {
 
 	public void setDepartment(String department) {
 		this.department = department;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(department, firstName, lastName);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		return Objects.equals(department, other.department) && Objects.equals(firstName, other.firstName)
+				&& Objects.equals(lastName, other.lastName);
 	}
 	
 	
